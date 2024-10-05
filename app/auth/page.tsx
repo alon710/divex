b796a7177a6b519387d471ed97d5778adf86a603
Alon Barad <alon710@gmail.com>
2024-10-05 16:22:02 +0300

    refactor: Update SignIn page to redirect to profile if user is already authenticated

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,5 +1,15 @@
 import SignInForm from "@/components/auth/SignInForm";
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
 
-export default function SignIn() {
+export default async function SignIn() {
+  const supabase = createClient();
+
+  const { data } = await supabase.auth.getUser();
+
+  if (data?.user) {
+    redirect("/profile");
+  }
+
   return <SignInForm />;
 }
